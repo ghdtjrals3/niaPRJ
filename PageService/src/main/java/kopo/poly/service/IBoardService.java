abe262ec4fe169c249a7ff60628aed106a7f0464
ghdtjrals3 <ghdtjrals3@gmail.com>
2022-12-20 14:25:27 +0900

    최초 프로젝트

---
+++ PageService/src/main/java/kopo/poly/service/IBoardService.java
@@ -0,0 +1,7 @@
+package kopo.poly.service;
+
+
+public interface IBoardService {
+
+}
+
---
+++ PageService/src/main/java/kopo/poly/service/impl/BoardService.java
@@ -0,0 +1,14 @@
+package kopo.poly.service.impl;
+
+import kopo.poly.service.IBoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service("BoardService")
+public class BoardService implements IBoardService {
+
+
+}
